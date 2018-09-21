7b2a157f90dbd742c675a5d1635eb480ff3b5933
Dave Mills <DaveMills@asos.com>
2018-09-21 15:45:07 +0100

    updated to allow related items publish.  defaults to all languages if none specified

--- src/Sitecore.Ship.Core/Domain/PublishParameters.cs
+++ src/Sitecore.Ship.Core/Domain/PublishParameters.cs
@@ -2,6 +2,8 @@
 {
     public class PublishParameters
     {
+        public bool Related { get; set; }
+        public bool Deep { get; set; }
         public string Mode { get; set; }
         public string Source { get; set; }
         public string[] Targets { get; set; }
